cc408fb42db3253f69fbd7beec37f0522bcf84b0
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2021-10-17 15:20:57 +0800

    IDK

---
+++ cmake-build-debug/CMakeFiles/ncu_lesson.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ncu_lesson.dir/DS_HW1_task1.cpp.o"
+  "ncu_lesson"
+  "ncu_lesson.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ncu_lesson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
