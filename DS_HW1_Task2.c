43ea0d7f86c0e5805ef6da45e1b83d57d63dcd5d
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2021-10-18 21:54:20 +0800

    MDFK

--- DS_HW1_Task2.c
+++ DS_HW1_Task2.c
@@ -1,19 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<math.h>
 
 void toPostfix(char* infix, char* postfix);
 int calPostfix(char* postfix);
+int priority(char op);
 
 int main(void) {
-    char infix[10000], postfix[10000];
+    char infix[20000], postfix[40000];
     int sum;
 
     scanf("%s", infix);
     toPostfix(infix, postfix);
+    printf(">");
+    for (int i = 0; i < strlen(postfix); i++) {
+        printf(" %c", postfix[i]);
+    }
     sum = calPostfix(postfix);
 
-    printf("> %s\n> %d\n", postfix, sum);
+    printf("\n> %d\n", sum);
 
     return 0;
 }
@@ -24,61 +30,72 @@ void toPostfix(char* infix, char* postfix) {
 
     for(int i = 0; i < length; i++) {
         switch (infix[i]) {
+            case '(' :
+                operator[++top] = infix[i];
+                break;
+            case ')' :
+                while(operator[top] != '(') {
+                    postfix[cnt++] = operator[top--];
+                }
+                top--;
+                break;
             case '+' :
             case '-' :
             case '*' :
             case '/' :
-                operator[top++] = infix[i];
-                postfix[cnt++] = ' ';
-                break;
-            case ')' :
-                postfix[cnt++] = ' ';
-                for (int j = top-1; j > 0; j--) {
-                    if (operator[j] != '(') {
-                        top = j;
-                        break;
-                    }
+                while(priority(operator[top]) >= priority(infix[i])) {
+                    postfix[cnt++] = operator[top--];
                 }
-                postfix[cnt++] = operator[top];
-                break;
-            case '(' :
-                operator[top++] = infix[i];
+                operator[++top] = infix[i]; // 存入堆疊
                 break;
             default :
                 postfix[cnt++] = infix[i];
         }
     }
+    while(top > 0) {
+        postfix[cnt++] = operator[top--];
+    }
 };
 
 int calPostfix(char* postfix) {
     int length = strlen(postfix), top = 0;
-    int operand[(length+1)/2];
+    double operand[length];
 
     for (int i = 0; i < length; i++) {
-        if (postfix[i] != ' ') {
-            switch (postfix[i]) {
-                case '+' :
-                    operand[top-2] += operand[top-1];
-                    top -= 1;
-                    break;
-                case '-' :
-                    operand[top-2] -= operand[top-1];
-                    top -= 1;
-                    break;
-                case '*' :
-                    operand[top-2] *= operand[top-1];
-                    top -= 1;
-                    break;
-                case '/' :
-                    operand[top-2] /= operand[top-1];
-                    top -= 1;
-                    break;
-                default :
-                    operand[top] = (int)postfix[i] - 48;
-                    top++;
-            }
+        switch (postfix[i]) {
+            case '+' :
+                operand[top-2] += operand[top-1];
+                top--;
+                break;
+            case '-' :
+                operand[top-2] -= operand[top-1];
+                top--;
+                break;
+            case '*' :
+                operand[top-2] *= operand[top-1];
+                top--;
+                break;
+            case '/' :
+                operand[top-2] /= operand[top-1];
+                top--;
+                break;
+            default :
+                operand[top++] = ((int)postfix[i] - 48) / 1.0;
         }
     }
 
-    return operand[0];
+    return (int)round(operand[0]);
 };
+
+int priority(char op) {
+    switch(op) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        default:
+            return 0;
+    }
+}
