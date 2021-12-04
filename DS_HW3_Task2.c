6ccae7e4d66351752b510d98d57d7017977d8faf
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2021-12-04 21:01:24 +0800

    DS_HW3_Task2

--- DS_HW3_Task2.c
+++ DS_HW3_Task2.c
@@ -8,17 +8,26 @@ typedef struct binary_search_tree_node {
 } node;
 
 node *newNode(int val);
-
 node *insert(node *root, int val);
-
 void preorderTraversal(node *root);
-
 void postorderTraversal(node *root);
-
 void clear(node *root);
 
 int main(void) {
+    int num, value;
+    node *root = NULL;
 
+    scanf("%d", &num);
+    for (int i = 0; i < num; i++) {
+        scanf("%d", &value);
+        insert(root, value);
+    }
+    printf("> Preorder:");
+    preorderTraversal(root);
+    printf("\n");
+    printf("> Postorder:");
+    postorderTraversal(root);
+    printf("\n");
 
     return 0;
 }
@@ -35,13 +44,35 @@ node *newNode(int val) {
 };
 
 node *insert(node *root, int val) {
+    node *newNode = newNode(val);
+    node *cur, *parent;
 
+    if (root == NULL) {
+        root = newNode;
+    } else {
+        cur = root;
+        while (cur != NULL) {
+            cur = parent;
+            if (val < cur->data) {
+                cur = cur->left;
+            } else {
+                cur = cur->right;
+            }
+        }
+        if (val < parent->data) {
+            parent->left = newNode;
+        } else {
+            parent->right = newNode;
+        }
+    }
+
+    return root;
 };
 
 void preorderTraversal(node *root) {
     node *cur = root;
     if (cur != NULL) {
-        printf("%d", cur->data);
+        printf(" %d", cur->data);
         preorder(cur->left);
         preorder(cur->right);
     }
@@ -52,7 +83,7 @@ void postorderTraversal(node *root) {
     if (cur != NULL) {
         preorder(cur->left);
         preorder(cur->right);
-        printf("%d", cur->data);
+        printf(" %d", cur->data);
     }
 };
 
