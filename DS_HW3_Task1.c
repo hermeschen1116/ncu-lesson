f19b1973edbda91d3b351add247cf7d6a3874dce
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2021-12-05 09:39:53 +0800

    DS_HW3_Task1

--- DS_HW3_Task1.c
+++ DS_HW3_Task1.c
@@ -1,95 +1,95 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-typedef struct binary_search_tree_node {
-    int data;
+typedef struct huffman_tree_node {
+    char letter;
+    int frequency;
     struct binary_search_tree_node *left;
     struct binary_search_tree_node *right;
 } node;
 
-node *newNode(int val);
-node *insert(node *root, int val);
-void preorderTraversal(node *ptr);
-void postorderTraversal(node *ptr);
+void swap(char *s1, char *s2);
+void sortString(char source[], char dest[]);
+node *countFrequency(char source[], node *frequencyQueue);
+node *newNode(char letter, int letterFrequency);
 void clear(node *root);
 
 int main(void) {
-    int num, value;
-    node *root = NULL;
+    char src[] = "aadjjcllsl", dest[strlen(src)];
+    node *froot = NULL, *cur;
 
-    scanf("%d", &num);
-    for (int i = 0; i < num; i++) {
-        scanf("%d", &value);
-        root = insert(root, value);
-    }
-    printf("> Preorder:");
-    preorderTraversal(root);
-    printf("\n> Postorder:");
-    postorderTraversal(root);
-    printf("\n");
-
-    clear(root);
+    froot = countFrequency(src, froot);
+    cur = froot;
+    /*while (cur != NULL) {
+        print("(%c, %d)\n", cur->letter, cur->frequency);
+        cur = cur->right;
+    }*/
 
     return 0;
 }
 
-node *newNode(int val) {
+void swap(char *s1, char *s2){
+    char temp;
+    temp = *s1;
+    *s1 = *s2;
+    *s2 = temp;
+}
+
+void sortString(char source[], char dest[]) {
+    int length = strlen(source);
+
+    strcpy(dest, source);
+    for (int i = length; i > 1; i--){
+        for (int j = 0; j < i-1; j++){
+            if (strcmp(&dest[j+1], &dest[j]) < 0){
+                swap(&dest[j+1], &dest[j]);
+            }
+        }
+    }
+}
+
+node *countFrequency(char source[], node *frequencyQueue) {
+    int length = strlen(source), count = 0;
+    char target, dest[length];
+    node *cur = frequencyQueue, *input;
+
+    sortString(source, dest);
+    target = dest[0];
+    for(int i = 0; i < length; i++) {
+        if (strcmp(&dest[i], &target) == 0) {
+            ++count;
+        } else {
+            input = newNode(target, count);
+            if (frequencyQueue == NULL) {
+                frequencyQueue = input;
+            } else {
+
+            }
+            count = 1;
+            target = dest[i];
+        }
+    }*/
+
+    return frequencyQueue;
+}
+
+node *newNode(char letter, int letterFrequency) {
     node *new_node;
 
     new_node = (node *) malloc(sizeof(node));
-    new_node->data = val;
+    new_node->letter = letter;
+    new_node->frequency = letterFrequency;
     new_node->left = NULL;
     new_node->right = NULL;
 
     return new_node;
 }
 
-node *insert(node *root, int val) {
-    node *input = newNode(val);
-    node *cur, *parent;
-
-    if (root == NULL) {
-        root = input;
-    } else {
-        cur = root;
-        while (cur != NULL) {
-            parent = cur;
-            if (val < cur->data) {
-                cur = cur->left;
-            } else {
-                cur = cur->right;
-            }
-        }
-        if (val < parent->data) {
-            parent->left = input;
-        } else {
-            parent->right = input;
-        }
-    }
-
-    return root;
-}
-
-void preorderTraversal(node *ptr) {
-    if (ptr != NULL) {
-        printf(" %d", ptr->data);
-        preorderTraversal(ptr->left);
-        preorderTraversal(ptr->right);
-    }
-}
-
-void postorderTraversal(node *ptr) {
-    if (ptr != NULL) {
-        postorderTraversal(ptr->left);
-        postorderTraversal(ptr->right);
-        printf(" %d", ptr->data);
-    }
-}
-
-void clear(node *root) {
+/*void clear(node *root) {
     if (root != NULL) {
         clear(root->left);
         clear(root->right);
         free(root);
     }
-}
+}*/
