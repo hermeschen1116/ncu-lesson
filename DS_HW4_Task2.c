6f45e6e3bb2a9786d81de252ffd36c763e5efd8f
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2021-12-29 03:31:27 +0800

    DS_HW4_Task2 partial

--- DS_HW4_Task2.c
+++ DS_HW4_Task2.c
@@ -1,8 +1,133 @@
 #include<stdio.h>
-#include<stdlib.h>
+
+int isConnected(int aMatrix[256][256], int size);
+int isAllAppended(int status[256], int size);
+int findMinWeight(int edges[256], int status[256], int size);
+int findParent(int path[256][256], int size, int *cnt, int end);
+int getWeight(int aMatrix[256][256], int size);
 
 int main(void) {
+    int size, aMatrix[256][256] = {0}, weight;
 
+    scanf("%d", &size);
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            scanf("%d", &aMatrix[i][j]);
+        }
+    }
+
+    if (isConnected(aMatrix, size)) {
+        weight = getWeight(aMatrix, size);
+        printf("> %d\n", weight);
+    } else {
+        printf("> NO connected\n");
+    }
 
     return 0;
 }
+
+int isConnected(int aMatrix[256][256], int size) {
+    int nonZeroCnt, pairCnt = 0;
+
+    for (int i = 1; i < size; i++) {
+        nonZeroCnt = 0;
+        for (int j = 0; j < i; j++) {
+            if (aMatrix[i][j] != 0) {
+                nonZeroCnt++;
+            }
+        }
+        if (nonZeroCnt > 0) {
+            pairCnt++;
+        }
+    }
+    if (pairCnt == size-1) {
+        return 1;
+    }
+    return 0;
+}
+
+int isAllAppended(int status[256], int size) {
+    for (int i = 0; i < size; i++) {
+        if (status[i] == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int findMinWeight(int edges[256], int status[256], int size) {
+    int min[2] = {-1, 10001}, flag = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (edges[i] != 0 && status[i] == 0) {
+            if (edges[i] < min[1]) {
+                min[0] = i;
+                min[1] = edges[i];
+                flag = 1;
+            }
+            if (edges[i] == min[1]) {
+                if (i < min[0]) {
+                    min[0] = i;
+                    min[1] = edges[i];
+                    flag = 1;
+                }
+            }
+        }
+    }
+
+    return flag ? min[0] : -1;
+}
+
+int findParent(int path[256][256], int size, int *cnt, int end) {
+    int parent;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (path[i][j] == *cnt*2+end) {
+                path[i][j] = 0;
+            }
+            if (path[i][j] == (*cnt-1)*2+end) {
+                *cnt -= 1;
+                parent = i;
+                break;
+            }
+        }
+    }
+
+    return parent;
+}
+
+int getWeight(int aMatrix[256][256], int size) {
+    int status[256] = {0}, path[256][256] = {0}, sCnt = 0, eCnt = 0;
+    int start = 0, end = 0, weight = 0, nextVertex, flag = 1;
+
+    status[0] = 1;
+    while (isAllAppended(status, size) == 0) {
+        if (flag) {
+            nextVertex = findMinWeight(aMatrix[end], status, size);
+            if (nextVertex != -1) {
+                weight += aMatrix[end][nextVertex];
+                path[end][nextVertex] = eCnt*2+1;
+                ++eCnt;
+                end = nextVertex;
+            } else {
+                end = findParent(path, size, &eCnt, 1);
+                flag = 0;
+            }
+        } else {
+            nextVertex = findMinWeight(aMatrix[start], status, size);
+            if (nextVertex != -1) {
+                weight += aMatrix[start][nextVertex];
+                path[start][nextVertex] = sCnt*2;
+                ++sCnt;
+                start = nextVertex;
+            } else {
+                start = findParent(path, size, &sCnt, 0);
+                flag = 1;
+            }
+        }
+        status[nextVertex] = 1;
+    }
+
+    return weight;
+}
+
--- DS_HW4_Task3.c
+++ DS_HW4_Task3.c
@@ -1,8 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int restoreMatrix
+int isConnected(int aMatrix[32][32], int size);
+
 int main(void) {
+    int m, n, aMatrix[32][32] = {0}, weight = 0;
 
+    scanf("%d %d", &m, &n);
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            scanf("%d", &aMatrix[i][j]);
+        }
+    }
 
+    if (isConnected(aMatrix, size)) {
+        printf("> %d\n", weight);
+    } else {
+        printf("> NO connected\n");
+    }
+
+    return 0;
+}
+
+int isConnected(int aMatrix[32][32], int size) {
+    int nonZeroCnt, pairCnt = 0;
+
+    for (int i = 1; i < size; i++) {
+        nonZeroCnt = 0;
+        for (int j = 0; j < i; j++) {
+            if (aMatrix[i][j] != 0) {
+                nonZeroCnt++;
+            }
+        }
+        if (nonZeroCnt > 0) {
+            pairCnt++;
+        }
+    }
+    if (pairCnt == size-1) {
+        return 1;
+    }
     return 0;
 }
