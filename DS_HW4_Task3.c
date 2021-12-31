975817a723689bae5eb07508eab28e86208d9fc5
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2021-12-31 22:32:25 +0800

    styled the variable's name

--- DS_HW4_Task3.c
+++ DS_HW4_Task3.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
-int isConnected(int aMatrix[128][128], int size);
-void showAdjacencyMatrix(int aMatrix[][128], int size);
+int isConnected(int a_matrix[128][128], int size);
+void showAdjacencyMatrix(int a_matrix[][128], int size);
 void swap(int *data_1, int *data_2);
 void swapEdge(int edge_1[3], int edge_2[3]);
 void sortEdges(int edges[128][3], int size);
 int isAllAppended(int status[128], int size);
-int getWeight(int edgeList[128][3], int line, int size);
+int getWeight(int edges_list[128][3], int line, int size);
 
 int main(void) {
     int size, line, edgeList[128][3] = {0}, aMatrix[128][128] = {0}, weight;
@@ -31,31 +31,31 @@ int main(void) {
     return 0;
 }
 
-int isConnected(int aMatrix[128][128], int size) {
-    int nonZeroCnt, pairCnt = 0;
+int isConnected(int a_matrix[128][128], int size) {
+    int nonZero_cnt, pair_cnt = 0;
 
     for (int i = 1; i < size; i++) {
-        nonZeroCnt = 0;
+        nonZero_cnt = 0;
         for (int j = 0; j < i; j++) {
-            if (aMatrix[i][j] != 0) {
-                nonZeroCnt++;
+            if (a_matrix[i][j] != 0) {
+                nonZero_cnt++;
             }
         }
-        if (nonZeroCnt > 0) {
-            pairCnt++;
+        if (nonZero_cnt > 0) {
+            pair_cnt++;
         }
     }
-    if (pairCnt == size-1) {
+    if (pair_cnt == size-1) {
         return 1;
     }
     return 0;
 }
 
-void showAdjacencyMatrix(int aMatrix[][128], int size) {
+void showAdjacencyMatrix(int a_matrix[][128], int size) {
     for (int i = 0; i < size; i++) {
-        printf("%d", aMatrix[i][0]);
+        printf("%d", a_matrix[i][0]);
         for (int j = 1; j < size; j++) {
-            printf(" %d", aMatrix[i][j]);
+            printf(" %d", a_matrix[i][j]);
         }
         printf("\n");
     }
@@ -94,27 +94,27 @@ int isAllAppended(int status[128], int size) {
     return 1;
 }
 
-int getWeight(int edgeList[128][3], int line, int size) {
+int getWeight(int edges_list[128][3], int line, int size) {
     int status[128] = {0}, weight, start, end;
 
-    sortEdges(edgeList, line);
+    sortEdges(edges_list, line);
     for (int i = 0; i < line; i++) {
-        start = status[edgeList[i][0]];
-        end = status[edgeList[i][1]];
+        start = status[edges_list[i][0]];
+        end = status[edges_list[i][1]];
         if (isAllAppended(status, size) == 0) {
             if (start*end == 0) {
                 if (start - end == 0) {
-                    weight += edgeList[i][2];
-                    status[edgeList[i][0]] = 1;
-                    status[edgeList[i][1]] = 1;
+                    weight += edges_list[i][2];
+                    status[edges_list[i][0]] = 1;
+                    status[edges_list[i][1]] = 1;
                 }
                 if (start - end == -1) {
-                    weight += edgeList[i][2];
-                    status[edgeList[i][0]] = 1;
+                    weight += edges_list[i][2];
+                    status[edges_list[i][0]] = 1;
                 }
                 if (start - end == 1) {
-                    weight += edgeList[i][2];
-                    status[edgeList[i][1]] = 1;
+                    weight += edges_list[i][2];
+                    status[edges_list[i][1]] = 1;
                 }
             }
         }
