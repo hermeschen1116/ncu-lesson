30c8891088b215668773fb8f3276dcf1bfb0f043
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2022-03-09 00:09:33 +0800

    ALG_HW2_Task1

---
+++ ALG_HW2_Task1.py
@@ -0,0 +1,15 @@
+def swapCount(_arr_, _q_) :
+    swapCnt = 0
+    for i in range(_q_) :
+        for j in range(i, _q_) :
+            if (_arr_[i] > _arr_[j]) :
+                _arr_[i], _arr_[j] = _arr_[j], _arr_[i]
+                swapCnt += 1
+    return swapCnt
+
+n = int(input())
+for i in range(n) :
+    q = int(input())
+    src = list(map(int, input().split(' ')))
+    print("Optimal swapping takes {} swaps.".format(swapCount(src, q)))
+    print(src)
