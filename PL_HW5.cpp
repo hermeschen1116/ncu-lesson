2b545c848c9ba8cfda60df9348def690321f63bb
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2022-04-17 15:57:01 +0800

    PL_HW5 finished

--- PL_HW5.cpp
+++ PL_HW5.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+
 typedef struct card_t {
 	int number;
 	char suit;
@@ -8,7 +9,6 @@ typedef struct card_t {
 
 typedef struct card_node {
 	card_t card;
-    struct card_node *pair;
 	struct card_node *next;
 } node;
 
@@ -16,10 +16,12 @@ void getCard(char const input[], char *suit, int *number);
 node *newNode(char suit, int number);
 node *push(node *stack, char suit, int number);
 node *pop(node *stack);
+bool nodeCompare(node *n1, node *n2);
 node *search(node *cards, node *input);
 node *insert(node *cards, node *input);
-node *searchPair(node *stack);
-void remove(node *cards);
+bool isPair(node *card);
+node *remove2(node *card1);
+node *removePairs(node *cards);
 void show(node *cards);
 void clear(node *head);
 
@@ -27,7 +29,7 @@ int main() {
     char input[3];
     char suit;
     int number, cnt = 0;
-	node *stack = nullptr, *cards = nullptr, *tmp, *pick;
+	node *stack = nullptr, *cards = nullptr, *pick;
 	
 	while (cin >> input && cnt < 13) {
         getCard(input, &suit, &number);
@@ -35,15 +37,13 @@ int main() {
         cnt++;
     }
 
-    tmp = stack;
-    while (tmp != nullptr) {
+    while (stack != nullptr) {
         pick = newNode(stack->card.suit, stack->card.number);
         stack = pop(stack);
         cards = insert(cards, pick);
-        tmp = tmp->next;
     }
 
-    remove(cards);
+    cards = removePairs(cards);
     show(cards);
     clear(stack);
     clear(cards);
@@ -62,7 +62,6 @@ node *newNode(char suit, int number) {
 
     tmp->card.suit = suit;
     tmp->card.number = number;
-    tmp->pair = nullptr;
     tmp->next = nullptr;
 
     return tmp;
@@ -86,20 +85,22 @@ node *pop(node *stack) {
     return stack;
 }
 
-node *search(node *cards, node *input) {
-    node *cur = cards, *next = cur->next;
+bool nodeCompare(node *n1, node *n2) {
+    if (n1->card.number < n2->card.number)  return false;
+    if (n1->card.number == n2->card.number && n1->card.suit < n2->card.suit) return false;
+    return true;
+}
 
-    while (next != nullptr) {
-        if (input->card.number < next->card.number) break;
-        if (input->card.number == next->card.number && input->card.suit < next->card.suit) {
-            input->pair = next;
-            break;
-        }
-        cur = next;
-        next = next->next;
+node *search(node *cards, node *input) {
+    node *pre = nullptr, *cur = cards;
+
+    while (cur != nullptr) {
+        if (!nodeCompare(input, cur)) break;
+        pre = cur;
+        cur = cur->next;
     }
 
-    return cur;
+    return pre;
 }
 
 node *insert(node *cards, node *input) {
@@ -108,36 +109,59 @@ node *insert(node *cards, node *input) {
     if (cards == nullptr) cards = input;
     else {
         target = search(cards, input);
-        if (target->next != nullptr) input->next = target->next;
-        target->next = input;
+        if (target == nullptr) {
+            input->next = cards;
+            cards = input;
+        } else {
+            if (target->next != nullptr) input->next = target->next;
+            target->next = input;
+        }
     }
 
     return cards;
 }
 
-node *searchPair(node *stack) {
-    node *tmp = stack;
+bool isPair(node *card) {
+    node *nextCard = card->next;
 
-    while (tmp != nullptr) {
-        if (tmp->pair != nullptr) break;
-        tmp = tmp->next;
-    }
-
-    return tmp;
+    if (nextCard != nullptr && nextCard->card.number == card->card.number) return true;
+    return false;
 }
 
-void remove(node *cards) {
-    node *cur = cards, *target;
+node *remove2(node *card1) {
+    node *card2 = card1->next, *card3 = nullptr;
 
-    while (cur != nullptr) {
-        target = searchPair(cur);
-        if (target != nullptr) {
-            cur->next = target->pair->next;
-            delete target->pair;
-            delete target;
-        } else break;
-        cur = cur->next;
+    if (card2 != nullptr) {
+        if (card2->next != nullptr) card3 = card2->next;
+        delete card2;
+        delete card1;
     }
+
+    return card3;
+}
+
+node *removePairs(node *cards) {
+    node *pre = cards, *cur = cards;
+
+    if (cards != nullptr) {
+        while (cur != nullptr) {
+            if (isPair(cur)) {
+                if (cur == cards) {
+                    cards = remove2(cards);
+                    pre = cards;
+                    cur = pre;
+                } else {
+                    pre->next = remove2(cur);
+                    if (pre->next != nullptr) cur = pre->next;
+                }
+            } else {
+                pre = cur;
+                cur = cur->next;
+            }
+        }
+    }
+
+    return cards;
 }
 
 void show(node *cards) {
