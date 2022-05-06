16711aacb612618e6d43607ddfd217ff339090a9
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2022-05-06 09:22:45 +0800

    ALG_HW6_Task3 finished

--- ALG_HW6_Task3.cpp
+++ ALG_HW6_Task3.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-int getMinArrayDifference(vector<int> _array_);
+int getMinArrayDifference(vector<long long int> _array_);
 
 int main() {
     int nData, arraySize, buffer;
-    vector<int> array;
+    vector<long long int> array;
 
     cin >> nData;
     for (int i = 0; i < nData; i++) {
@@ -22,9 +23,10 @@ int main() {
     return 0;
 }
 
-int getMinArrayDifference(vector<int> _array_) {
-    int length = _array_.size();
-    int left = 0, right = 0, minIndex = 0, minD, curD;
+int getMinArrayDifference(vector<long long int> _array_) {
+    int length = _array_.size(), minIndex = 0;
+    long long int left = 0, right = 0;
+    double minD, curD;
 
     if (length == 1) return 0;
     for (int i = 0; i < length; i++) right += _array_[i];
@@ -32,8 +34,9 @@ int getMinArrayDifference(vector<int> _array_) {
     for (int i = 0; i < length; i++) {
         left += _array_[i];
         right -= _array_[i];
-        curD = left / (i+1);
-        if (i < length-1) curD = abs(curD - right / (length-i-1));
+        curD = floor(left / (i+1));
+        if (i < length-1) curD -= floor(right / (length-i-1));
+        curD = abs(curD);
         if (curD < minD) {
             minD = curD;
             minIndex = i;
