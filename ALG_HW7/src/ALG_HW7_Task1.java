d688a6e07be6757401d8142013ea55496f4db59f
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2022-05-12 02:07:57 +0800

    finish task 1

--- ALG_HW7/src/ALG_HW7_Task1.java
+++ ALG_HW7/src/ALG_HW7_Task1.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class ALG_HW7_Task1 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int nData;
+        double exp, powered;
 
+        nData = input.nextInt();
+        for (int i = 0; i < nData; i++) {
+            exp = input.nextDouble();
+            powered = input.nextDouble();
+            System.out.printf("%.0f\n", Math.pow(powered, 1/exp));
+        }
     }
 }
