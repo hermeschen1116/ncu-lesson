4daf9e60d0acb3f7eb20211fafdac8545ed40cdf
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2022-05-20 00:52:47 +0800

    task2 finished

--- ALG_HW8/src/ALG_HW8_Task2.java
+++ ALG_HW8/src/ALG_HW8_Task2.java
@@ -1,34 +1,65 @@
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ALG_HW8_Task2 {
-    public static BigInteger power2Recursion(int _power_, ArrayList<BigInteger> _powerSet_) {
-        BigInteger multiplier = new BigInteger("2");
-        BigInteger tmp;
+    public static ArrayList<Integer> multiplyBigNumber(ArrayList<Integer> _multiplicand_, int _smallMultiplier_) {
+        ArrayList<Integer> result = new ArrayList<>(_multiplicand_);
+        int length = result.size(), buffer, carry = 0;
+
+        for (int i = 0; i < length; i++) {
+            buffer = result.get(i) * _smallMultiplier_ + carry;
+            result.set(i, buffer % 10);
+            carry = buffer / 10;
+            if (i == length - 1 && carry != 0) result.add(carry);
+        }
+
+        return result;
+    }
+
+    public static ArrayList<Integer> sumBigNumber(ArrayList<Integer> _n1_, ArrayList<Integer> _n2_) {
+        ArrayList<Integer> result;
+        int buffer, carry = 0, length;
+
+        if (_n1_.size() > _n2_.size()) {
+            result = new ArrayList<>(_n1_);
+            for (int i = 0; i < _n2_.size(); i++) result.set(i, result.get(i) + _n2_.get(i));
+        } else {
+            result = new ArrayList<>(_n2_);
+            for (int i = 0; i < _n1_.size(); i++) result.set(i, result.get(i) + _n1_.get(i));
+        }
+
+        length = result.size();
+        for (int i = 0; i < length; i++) {
+            buffer = result.get(i) + carry;
+            result.set(i, buffer % 10);
+            carry = buffer / 10;
+            if (i == length - 1 && carry != 0) result.add(carry);
+        }
+
+        return result;
+    }
+
+    public static ArrayList<Integer> power2Recursion(int _power_, ArrayList<ArrayList<Integer>> _powerSet_) {
+        ArrayList<Integer> tmp = new ArrayList<>();
 
         if (_power_ < _powerSet_.size()) tmp = _powerSet_.get(_power_);
         else {
-            if (_power_ == 0) tmp = new BigInteger("1");
-            else tmp = power2Recursion(_power_ - 1, _powerSet_).multiply(multiplier);
+            if (_power_ == 0) tmp.add(1);
+            else tmp = multiplyBigNumber(power2Recursion(_power_ - 1, _powerSet_), 2);
             _powerSet_.add(tmp);
         }
         return tmp;
     }
 
-    public static BigInteger sum2Exponent(int _power1_, int _power2_) {
-        ArrayList<BigInteger> powerSet = new ArrayList<>();
-        int powerD = Math.abs(_power1_ - _power2_);
-        BigInteger sum;
-        BigInteger tmp;
+    public static String sum2Exponent(int _power1_, int _power2_) {
+        ArrayList<ArrayList<Integer>> powerSet = new ArrayList<>();
+        ArrayList<Integer> sum;
+        StringBuilder result = new StringBuilder();
 
-        sum = power2Recursion(Math.min(_power1_, _power2_), powerSet);
-        if (powerD > powerSet.size()) tmp = power2Recursion(powerD, powerSet);
-        else tmp = powerSet.get(powerD);
-        tmp = tmp.multiply(sum);
-        sum = sum.add(tmp);
+        sum = sumBigNumber(power2Recursion(_power1_, powerSet), power2Recursion(_power2_, powerSet));
+        for (int i = sum.size() - 1; i >= 0; i--) result.append(sum.get(i));
 
-        return sum;
+        return result.toString();
     }
 
     public static void main(String[] args) {
