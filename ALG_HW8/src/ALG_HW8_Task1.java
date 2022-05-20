e522aeb909981b8da1be09a09324df06dbf9c213
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2022-05-21 02:58:10 +0800

    modified ALG_HW8_Task1

--- ALG_HW8/src/ALG_HW8_Task1.java
+++ ALG_HW8/src/ALG_HW8_Task1.java
@@ -1,56 +1,142 @@
 import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
-import java.util.HashMap;
-import java.util.Scanner;
-
-class node {
-    private char letter;
-    private int count;
-    private String code;
-    private node left, right;
-
-    public node (char _letter_, int _count_) {
-        this.letter = _letter_;
-        this.count = _count_;
-        this.code = "";
-    }
-
-    public void setLeftNode (node _left_) {
-        this.left = _left_;
-    }
-
-    public void setRightNode (node _right_) {
-        this.right = _right_;
-    }
-}
-
-class huffmanTree {
-    
-}
+import java.util.*;
 
 public class ALG_HW8_Task1 {
-    public static HashMap<Character, Integer> countLetter (String _plaintext_) {
-        HashMap<Character, Integer> letterTable = new HashMap<Character, Integer>();
-        CharacterIterator letter = new StringCharacterIterator(_plaintext_);
-        char tmp;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String plaintext;
+        ArrayList<node> tmp;
+        node root;
 
-        while (letter.current() != CharacterIterator.DONE)
-        {
-            tmp = letter.current();
-            if (letterTable.containsKey(tmp)) letterTable.computeIfPresent(tmp, (key, value) -> value + 1);
-            else letterTable.putIfAbsent(tmp, 1);
+        plaintext = input.next();
+        tmp = getFrequencyTable(plaintext);
+        //for (ALG_HW8_Task1.node node : tmp) System.out.println(node.toString());
+        root = buildHuffmanTree(tmp);
+        showHuffmanTree(root);
+    }
+
+    public static ArrayList<node> getFrequencyTable(String _plaintext_) {
+        HashMap<Character, node> countBuffer = new HashMap<>();
+        CharacterIterator letter = new StringCharacterIterator(_plaintext_);
+        char current;
+        ArrayList<node> frequencyTable = new ArrayList<>();
+
+        while (letter.current() != CharacterIterator.DONE) {
+            current = letter.current();
+            if (countBuffer.containsKey(current))
+                countBuffer.computeIfPresent(current, (k, v) -> new node("" + k, v.getRate() + 1));
+            else countBuffer.putIfAbsent(current, new node("" + current, 1));
             letter.next();
         }
 
-        return letterTable;
+        countBuffer.forEach((k, v) -> frequencyTable.add(v));
+        frequencyTable.sort(node.compare);
+
+        return frequencyTable;
     }
 
+    public static node buildHuffmanTree(ArrayList<node> _frequencyTable_) {
+        node root;
+        while (_frequencyTable_.size() > 1) {
+            _frequencyTable_.set(0, _frequencyTable_.get(0).merge(_frequencyTable_.get(1)));
+            System.out.print("tree:");
+            showHuffmanTree(_frequencyTable_.get(0));
+            _frequencyTable_.remove(1);
+            _frequencyTable_.sort(node.compare);
+        }
+        root = _frequencyTable_.get(0);
+        System.out.println("root:"+root.toString());
 
-    public static void main (String[] args) {
-        Scanner input = new Scanner(System.in);
-        String plaintext;
+        return root;
+    }
 
-        plaintext = input.next();
-        System.out.println(countLetter(plaintext));
+    public static void showHuffmanTree(node _node_) {
+        System.out.println(_node_.toString());
+        if (_node_.getLeft() != null) showHuffmanTree(_node_.getLeft());
+        if (_node_.getRight() != null) showHuffmanTree(_node_.getRight());
+    }
+
+    static class node {
+        static Comparator<node> compare = (_n1_, _n2_) -> {
+            int rate1 = _n1_.getRate();
+            int rate2 = _n2_.getRate();
+            if (rate1 != rate2) return rate1 < rate2 ? -1 : 1;
+            else return _n1_.getPlaintext().compareTo(_n2_.getPlaintext());
+        };
+        private String ciphertext;
+        private String plaintext;
+        private int rate;
+        private node left;
+        private node right;
+
+        public node(String _text_, int _rate_) {
+            plaintext = _text_;
+            ciphertext = "";
+            rate = _rate_;
+            left = null;
+            right = null;
+        }
+
+        String getPlaintext() {
+            return plaintext;
+        }
+
+        void setPlaintext(String _plaintext_) {
+            plaintext = _plaintext_;
+        }
+
+        void concatPlaintext(String _text_) {
+            setPlaintext(plaintext + _text_);
+        }
+
+        String getCiphertext() {
+            return ciphertext;
+        }
+        void setCiphertext (String _ciphertext_) {
+            ciphertext = _ciphertext_;
+        }
+        void concatCiphertext(String _text_) {
+            setCiphertext(ciphertext + _text_);
+        }
+
+        int getRate() {
+            return rate;
+        }
+
+        void computeRate(int _rate_) {
+            rate += _rate_;
+        }
+
+        node getLeft() {
+            return left;
+        }
+
+        void setLeft(node _node_) {
+            left = _node_;
+        }
+
+        node getRight() {
+            return right;
+        }
+
+        void setRight(node _node_) {
+            right = _node_;
+        }
+        @Override
+        public String toString() {
+            return "(" + plaintext + ", " + rate + ", " + ciphertext + ")";
+        }
+
+        node merge(node _node_) {
+            node merged = new node(plaintext, rate);
+
+            merged.concatPlaintext(_node_.plaintext);
+            merged.computeRate(_node_.getRate());
+            merged.setLeft(new node(plaintext, rate));
+            merged.setRight(new node(_node_.getPlaintext(), _node_.getRate()));
+
+            return merged;
+        }
     }
 }
--- ALG_HW8/src/ALG_HW8_Task3.java
+++ ALG_HW8/src/ALG_HW8_Task3.java
@@ -1,5 +1,76 @@
-public class ALG_HW8_Task3 {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class ALG_HW8_Task3 {
+    public static ArrayList<Integer> multiplyBigNumber(ArrayList<Integer> _multiplicand_, int _smallMultiplier_) {
+        ArrayList<Integer> result = new ArrayList<>(_multiplicand_);
+        int length = result.size(), buffer, carry = 0;
+
+        for (int i = 0; i < length; i++) {
+            buffer = result.get(i) * _smallMultiplier_ + carry;
+            result.set(i, buffer % 10);
+            carry = buffer / 10;
+            if (i == length - 1 && carry != 0) result.add(carry);
+        }
+
+        return result;
+    }
+
+    public static ArrayList<Integer> sumBigNumber(ArrayList<Integer> _n1_, ArrayList<Integer> _n2_) {
+        ArrayList<Integer> result;
+        int buffer, carry = 0, length;
+
+        if (_n1_.size() > _n2_.size()) {
+            result = new ArrayList<>(_n1_);
+            for (int i = 0; i < _n2_.size(); i++) result.set(i, result.get(i) + _n2_.get(i));
+        } else {
+            result = new ArrayList<>(_n2_);
+            for (int i = 0; i < _n1_.size(); i++) result.set(i, result.get(i) + _n1_.get(i));
+        }
+
+        length = result.size();
+        for (int i = 0; i < length; i++) {
+            buffer = result.get(i) + carry;
+            result.set(i, buffer % 10);
+            carry = buffer / 10;
+            if (i == length - 1 && carry != 0) result.add(carry);
+        }
+
+        return result;
+    }
+
+    public static ArrayList<Integer> power2Recursion(int _power_, ArrayList<ArrayList<Integer>> _powerSet_) {
+        ArrayList<Integer> tmp = new ArrayList<>();
+
+        if (_power_ < _powerSet_.size()) tmp = _powerSet_.get(_power_);
+        else {
+            if (_power_ == 0) tmp.add(1);
+            else tmp = multiplyBigNumber(power2Recursion(_power_ - 1, _powerSet_), 2);
+            _powerSet_.add(tmp);
+        }
+        return tmp;
+    }
+
+    public static String sum2Exponent(int _power1_, int _power2_) {
+        ArrayList<ArrayList<Integer>> powerSet = new ArrayList<>();
+        ArrayList<Integer> sum;
+        StringBuilder result = new StringBuilder();
+
+        sum = sumBigNumber(power2Recursion(_power1_, powerSet), power2Recursion(_power2_, powerSet));
+        for (int i = sum.size() - 1; i >= 0; i--) result.append(sum.get(i));
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int nData, m, n;
+
+        nData = input.nextInt();
+        for (int i = 0; i < nData; i++) {
+            m = input.nextInt();
+            n = input.nextInt();
+            System.out.println(sum2Exponent(m, n));
+        }
     }
 }
