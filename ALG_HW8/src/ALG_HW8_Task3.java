9c9b23569b1e36deefca95da31ba546c64274056
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2022-05-22 03:51:06 +0800

    ALG_HW8_Task3 finished but unknown error

--- ALG_HW8/src/ALG_HW8_Task3.java
+++ ALG_HW8/src/ALG_HW8_Task3.java
@@ -1,76 +1,140 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class ALG_HW8_Task3 {
-    public static ArrayList<Integer> multiplyBigNumber(ArrayList<Integer> _multiplicand_, int _smallMultiplier_) {
-        ArrayList<Integer> result = new ArrayList<>(_multiplicand_);
-        int length = result.size(), buffer, carry = 0;
-
-        for (int i = 0; i < length; i++) {
-            buffer = result.get(i) * _smallMultiplier_ + carry;
-            result.set(i, buffer % 10);
-            carry = buffer / 10;
-            if (i == length - 1 && carry != 0) result.add(carry);
-        }
-
-        return result;
-    }
-
-    public static ArrayList<Integer> sumBigNumber(ArrayList<Integer> _n1_, ArrayList<Integer> _n2_) {
-        ArrayList<Integer> result;
-        int buffer, carry = 0, length;
-
-        if (_n1_.size() > _n2_.size()) {
-            result = new ArrayList<>(_n1_);
-            for (int i = 0; i < _n2_.size(); i++) result.set(i, result.get(i) + _n2_.get(i));
-        } else {
-            result = new ArrayList<>(_n2_);
-            for (int i = 0; i < _n1_.size(); i++) result.set(i, result.get(i) + _n1_.get(i));
-        }
-
-        length = result.size();
-        for (int i = 0; i < length; i++) {
-            buffer = result.get(i) + carry;
-            result.set(i, buffer % 10);
-            carry = buffer / 10;
-            if (i == length - 1 && carry != 0) result.add(carry);
-        }
-
-        return result;
-    }
-
-    public static ArrayList<Integer> power2Recursion(int _power_, ArrayList<ArrayList<Integer>> _powerSet_) {
-        ArrayList<Integer> tmp = new ArrayList<>();
-
-        if (_power_ < _powerSet_.size()) tmp = _powerSet_.get(_power_);
-        else {
-            if (_power_ == 0) tmp.add(1);
-            else tmp = multiplyBigNumber(power2Recursion(_power_ - 1, _powerSet_), 2);
-            _powerSet_.add(tmp);
-        }
-        return tmp;
-    }
-
-    public static String sum2Exponent(int _power1_, int _power2_) {
-        ArrayList<ArrayList<Integer>> powerSet = new ArrayList<>();
-        ArrayList<Integer> sum;
-        StringBuilder result = new StringBuilder();
-
-        sum = sumBigNumber(power2Recursion(_power1_, powerSet), power2Recursion(_power2_, powerSet));
-        for (int i = sum.size() - 1; i >= 0; i--) result.append(sum.get(i));
-
-        return result.toString();
-    }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int nData, m, n;
+        int nInput;
+        String buffer;
+        ArrayList<Node> nodeInfo;
 
-        nData = input.nextInt();
-        for (int i = 0; i < nData; i++) {
-            m = input.nextInt();
-            n = input.nextInt();
-            System.out.println(sum2Exponent(m, n));
+        nInput = Integer.parseInt(input.nextLine());
+        input.nextLine();
+        for (int i = 0; i < nInput; i++) {
+            buffer = input.nextLine();
+            nodeInfo = getAllNodeInfo(buffer);
+            if (isValidTree(nodeInfo)) treeLevelTraversal(nodeInfo);
+            else System.out.println("not complete");
+        }
+    }
+
+    public static Node getNodeInfo(String infoBuffer) {
+        String[] info = infoBuffer.substring(1, infoBuffer.length() - 1).split(",");
+        if (info.length == 2) return new Node(Integer.parseInt(info[0]), info[1]);
+        else return new Node(Integer.parseInt(info[0]), "");
+    }
+
+    public static ArrayList<Node> getAllNodeInfo(String infoBuffer) {
+        ArrayList<Node> nodeInfo = new ArrayList<>();
+        String[] infos = infoBuffer.substring(0, infoBuffer.length() - 3).split(" ");
+
+        for (String s : infos) { nodeInfo.add(getNodeInfo(s)); }
+        nodeInfo.sort(Node.compareLevel);
+
+        return nodeInfo;
+    }
+
+    public static boolean isValidTree(ArrayList<Node> nodeInfo) {
+        ArrayList<Node> nodes = new ArrayList<>(nodeInfo);
+        Node root, cur;
+        String path;
+        boolean errorSignal = false;
+        int cnt = 1;
+
+        nodes.sort(Node.compare);
+        root = nodes.get(0);
+        nodes.remove(0);
+        if (!root.getPath().equals("")) { root = null; }
+        else {
+            for (Node n : nodes) {
+                path = n.getPath();
+                cur = root;
+                for (int i = 0; i < path.length(); i++) {
+                    if (i != path.length() - 1) {
+                        if (path.charAt(i) == 'L') {
+                            cur = cur.left;
+                        } else {
+                            cur = cur.right;
+                        }
+                    } else {
+                        if (path.charAt(i) == 'L') {
+                            if (cur.getLeft() == null) {
+                                cur.setLeft(n);
+                                ++cnt;
+                            } else {
+                                errorSignal = true;
+                                break;
+                            }
+                        } else {
+                            if (cur.getRight() == null) {
+                                cur.setRight(n);
+                                ++cnt;
+                            } else {
+                                errorSignal = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (errorSignal) { break; }
+            }
+            if (cnt != nodes.size() + 1) { root = null; }
+        }
+
+        return root != null;
+    }
+
+    public static void treeLevelTraversal(ArrayList<Node> nodeInfo) {
+        nodeInfo.sort(Node.compareLevel);
+        for (Node n :nodeInfo) System.out.print(n.getValue()+" ");
+        System.out.println();
+    }
+
+    static class Node {
+        static Comparator<Node> compare = Comparator.comparing(Node::getPath);
+        static Comparator<Node> compareLevel = (n1, n2) -> {
+            int pathLen1 = n1.getPath().length();
+            int pathLen2 = n2.getPath().length();
+            if (pathLen1 == pathLen2) return n1.getPath().compareTo(n2.getPath());
+            else {
+                return pathLen1 < pathLen2 ? -1 : 1;
+            }
+        };
+        private final int value;
+        private final String path;
+        private Node left;
+        private Node right;
+
+        public Node(int value, String path) {
+            this.value = value;
+            this.path = path;
+            left = null;
+            right = null;
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        String getPath() {
+            return path;
+        }
+
+        Node getLeft() {
+            return left;
+        }
+
+        void setLeft(Node node) {
+            left = node;
+        }
+
+        Node getRight() {
+            return right;
+        }
+
+        void setRight(Node node) {
+            right = node;
         }
     }
 }
