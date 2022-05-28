8be7ca2285896ebb36505d909c85aa021d60990e
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2022-05-28 18:58:55 +0800

    finish ALG_Final_Project but no multithread accelerate

--- ALG_Final_Project/Model.py
+++ ALG_Final_Project/Model.py
@@ -1,6 +1,88 @@
+import threading
+import time
+
 from keras import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from matplotlib import pyplot as plt
 
 
 class CNN:
-    def __init__(self):
+    def __init__(self, label_set, data_set):
         self.model = Sequential()
+        self.label_set = label_set
+        self.data_set = data_set
+        self.num_convolutions = 0
+        self.num_pooling = 0
+        self.num_dropout = 0
+        self.num_flatten = 0
+        self.num_dense = 0
+        self.train_logs = None
+        self.test_logs = []
+
+    def add_convolution_layer(self, filters, kernel_size, activation, input_shape):
+        if self.num_convolutions == 0:
+            self.model.add(Conv2D(filters, kernel_size, activation=activation, input_shape=input_shape))
+        else:
+            self.model.add(Conv2D(filters, kernel_size, activation=activation))
+        self.num_convolutions += 1
+
+    def add_pooling_layer(self, pool_size):
+        self.model.add(MaxPooling2D(pool_size=pool_size))
+        self.num_pooling += 1
+
+    def add_dropout_layer(self, dropout_rate):
+        self.model.add(Dropout(dropout_rate))
+        self.num_dropout += 1
+
+    def add_flatten_layer(self):
+        self.model.add(Flatten())
+        self.num_flatten += 1
+
+    def add_dense_layer(self, units, activation):
+        self.model.add(Dense(units, activation=activation))
+        self.num_dense += 1
+
+    def compile_model(self, loss, optimizer, metrics):
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+
+    def train_model(self, batch_size, epochs, verbose, validation_split):
+        timer_start = time.process_time()
+        self.train_logs = self.model.fit(self.data_set, self.label_set,
+                                         epochs=epochs, batch_size=batch_size, verbose=verbose,
+                                         validation_split=validation_split)
+        timer_end = time.process_time()
+        print('Training finished. Execution time: {:.2f} s'.format(timer_end - timer_start))
+
+    # def train_model_mt(self, batch_size, epochs, verbose, validation_split, num_threads=1):
+    #     threads = []
+    #     timer_start = time.time()
+    #     for i in range(num_threads):
+    #         threads.append(threading.Thread(target=self.train_model,
+    #                                         args=(batch_size, epochs, verbose, validation_split)))
+    #         threads[i].start()
+    #     thread = threading.Thread(target=self.train_model, args=(batch_size, epochs, verbose, validation_split))
+    #     thread.start()
+    #     thread.join()
+    #     for i in range(num_threads):
+    #         threads[i].join()
+    #     timer_end = time.time()
+    #     print('Training finished. Execution time: {:.2f} s'.format(timer_end - timer_start))
+
+    def show_train_logs(self):
+        plt.plot(self.train_logs.history['loss'])
+        plt.plot(self.train_logs.history['val_loss'])
+        plt.title('Training logs')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['loss', 'val_loss'], loc='upper left')
+        plt.show()
+
+    def test_model(self, label_set, data_set, verbose):
+        timer_start = time.process_time()
+        self.test_logs = self.model.evaluate(data_set, label_set, verbose=verbose)
+        timer_end = time.process_time()
+        print('Testing finished. Execution time: {:.2f} s'.format(timer_end - timer_start))
+
+    def show_test_logs(self):
+        print("Total loss: ", self.test_logs[0])
+        print("Total accuracy: ", self.test_logs[1])
--- ALG_Final_Project/Preprocess.py
+++ ALG_Final_Project/Preprocess.py
@@ -1,4 +1,6 @@
 import os
+import threading
+import time
 
 import numpy as np
 from PIL import Image
@@ -6,30 +8,32 @@ from keras.utils import np_utils
 
 
 class ImagePreprocessor:
-    def __init__(self, image_path, target_dimensions, color_mode):
-        self.image_path = image_path  # (path, filename)
-        self.target_dimensions = target_dimensions  # (channel, width, height)
-        self.color_mode = color_mode  # how many color
+    def __init__(self, image_path, data_dimensions, color_range):
+        self.image_path = image_path  # [path, filename]
+        self.data_dimensions = data_dimensions  # [width, height, channels, ...]
+        self.color_mode = color_range  # how many color
 
     def get_label(self):  # get label
-        return int(self.image_path[0].split('//')[2])
+        return int(self.image_path[0].split('/')[-1])
 
     def get_image_data(self):  # get processed image data
         full_path = os.path.join(self.image_path[0], self.image_path[1])
         image_data = Image.open(full_path)
 
-        return np.array(image_data / self.color_mode).reshape(self.target_dimensions)
+        return (np.array(image_data) / self.color_mode) \
+            .reshape(self.data_dimensions[2], self.data_dimensions[0], self.data_dimensions[1])
 
 
 class DataSet:
     def __init__(self, source_path, data_dimension, data_category):
         self.source_path = source_path  # set the data set source path
         self.source_size = 0  # initialize the number of images in the data set
+        self.data_dimension = data_dimension  # initialize the data dimension [width, height, channel, ...]
         self.data_category = data_category  # set the number of categories in the data set
-        self.data_dimension = data_dimension  # set the dimension of the data set (channel, width, height, ...)
+        self.file_path = []  # initialize the file path
         self.label_set = []  # initialize the label list
-        self.data_set = np.zeros(self.data_dimension[1], self.data_dimension[2]).reshape(self.data_dimension)
-        # initialize the data list
+        self.data_set = np.zeros((data_dimension[0], data_dimension[1])) \
+            .reshape(data_dimension[2], data_dimension[0], data_dimension[1])  # initialize the data list
 
     def set_source_path(self, source_path):  # set the data set source path
         self.source_path = source_path
@@ -43,28 +47,69 @@ class DataSet:
     def get_source_size(self):  # get the number of images in the data set
         return self.source_size
 
+    def set_data_dimensions(self, data_dimension):  # set the data dimension
+        self.data_dimension = data_dimension
+
+    def get_data_dimensions(self):  # get the data dimension
+        return self.data_dimension
+
     def set_data_category(self, data_category):  # set the number of categories in the data set
         self.data_category = data_category
 
     def get_data_category(self):  # get the number of categories in the data set
         return self.data_category
 
-    def set_data_dimension(self, data_dimension):  # set the dimension of the data set (channel, width, height, ...)
-        self.data_dimension = data_dimension
-
-    def get_data_dimension(self):  # get the dimension of the data set (channel, width, height, ...)
-        return self.data_dimension
-
-    def preprocess(self):  # preprocess the data set
+    def load_data_set(self):  # load the data set
+        timer_start = time.process_time()
         for root, dirs, files in os.walk(self.source_path):
             for file in files:
-                processor = ImagePreprocessor([root, file], self.data_dimension, 225)
-                self.label_set.append(processor.get_label())
-                self.data_set = np.vstack((self.data_set, processor.get_image_data()))
+                if file == '.DS_Store' or file == 'desktop.ini':
+                    continue
+                self.file_path.append([root, file])
                 self.source_size += 1
+        timer_end = time.process_time()
+        print('Data set loaded. Execution time: {:.2f} s'.format(timer_end - timer_start))
+
+    # def load_data_set_mt(self, num_threads=1):  # load the data set with multi-threading
+    #     threads = []
+    #     timer_start = time.process_time()
+    #     for i in range(num_threads):
+    #         threads.append(threading.Thread(target=self.load_data_set))
+    #         threads[i].start()
+    #     thread = threading.Thread(target=self.load_data_set)
+    #     thread.start()
+    #     thread.join()
+    #     for i in range(num_threads):
+    #         threads[i].join()
+    #     timer_end = time.process_time()
+    #     print('Data set loaded. Execution time: {:.2f} s'.format(timer_end - timer_start))
+
+    def preprocess(self):  # preprocess the data set
+        timer_start = time.process_time()
+        for path in self.file_path:
+            processor = ImagePreprocessor(path, self.data_dimension, 225)
+            self.label_set.append(processor.get_label())
+            self.data_set = np.vstack((self.data_set, processor.get_image_data()))
         self.data_set = np.delete(self.data_set, [0], 0)
-        self.data_set = self.data_set.reshape(self.source_size, self.data_dimension)
+        self.data_set = self.data_set.reshape(self.source_size,
+                                              self.data_dimension[0], self.data_dimension[1], self.data_dimension[2])
         self.label_set = np_utils.to_categorical(self.label_set, self.data_category)
+        timer_end = time.process_time()
+        print('Data set preprocessed. Execution time: {:.2f} s'.format(timer_end - timer_start))
+
+    # def preprocess_mt(self, num_threads=1):  # preprocess the data set with multi-threading
+    #     threads = []
+    #     timer_start = time.process_time()
+    #     for i in range(num_threads):
+    #         threads.append(threading.Thread(target=self.preprocess))
+    #         threads[i].start()
+    #     thread = threading.Thread(target=self.preprocess)
+    #     thread.start()
+    #     thread.join()
+    #     for i in range(num_threads):
+    #         threads[i].join()
+    #     timer_end = time.process_time()
+    #     print('Data set preprocessed. Execution time: {:.2f} s'.format(timer_end - timer_start))
 
     def get_label_set(self):  # get the processed label set
         return self.label_set
