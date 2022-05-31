bbcd8f8a9e0b46cbd13582575b3e36fbf4d254a1
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2022-05-31 17:22:10 +0800

    finished(maybe?) PL_HW10

--- PL_HW10.cpp
+++ PL_HW10.cpp
@@ -4,17 +4,56 @@
 #include <string>
 #include <thread>
 #include <ctime>
+
 using namespace std;
 clock_t tSTART;
 clock_t tEND;
 
-string exec(const char* cmd) {
+string exec(const char *cmd);
+
+int main() {
+
+    cout << "start trace" << endl;
+    // #1 trace
+    tSTART = clock();
+    exec("traceroute 1.1.1.1");
+    tEND = clock();
+    cout << "#1 trace " << (tEND - tSTART) / CLOCKS_PER_SEC << " sec" << endl;
+    // #2
+    tSTART = clock();
+    exec("traceroute 8.8.8.8");
+    tEND = clock();
+    cout << "#2 trace " << (tEND - tSTART) / CLOCKS_PER_SEC << " sec" << endl;
+    // #3
+    tSTART = clock();
+    exec("traceroute 168.95.1.1");
+    tEND = clock();
+    cout << "#3 trace " << (tEND - tSTART) / CLOCKS_PER_SEC << " sec" << endl;
+
+
+    cout << endl;
+    tSTART = clock();
+    // thread
+    auto ping1 = thread(exec, "traceroute 1.1.1.1");
+    auto ping2 = thread(exec, "traceroute 8.8.8.8");
+    auto ping3 = thread(exec, "traceroute 168.95.1.1");
+    ping1.join();
+    ping2.join();
+    ping3.join();
+    tEND = clock();
+    cout << "using thead " << (tEND - tSTART) / CLOCKS_PER_SEC << " sec" << endl;
+
+    return 0;
+}
+
+string exec(const char *cmd) {
     char buffer[128];
     string result;
-    FILE* pipe = popen(cmd, "r");
+    FILE *pipe = popen(cmd, "r");
+
     if (!pipe) throw runtime_error("popen() failed!");
     try {
-        while (fgets(buffer, sizeof buffer, pipe) != nullptr) {
+        while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
             result += buffer;
         }
     } catch (...) {
@@ -23,41 +62,6 @@ string exec(const char* cmd) {
     }
     pclose(pipe);
 
-    cout<<"task {"<<cmd<<"} finish!"<<endl;
+    cout << "task {" << cmd << "} finish!" << endl;
     return result;
-}
-
-int main(){
-
-    cout<<"start trace\n";
-    // #1 trace
-	tSTART=clock(); 
-	exec("traceroute 1.1.1.1");
-	tEND=clock();
-  	cout<<"#1 trace "<<  (tEND - tSTART) / CLOCKS_PER_SEC  <<" sec"<<endl;
-  	// #2
-    tSTART=clock();
-    exec("traceroute 8.8.8.8");
-    tEND=clock();
-    cout<<"#2 trace "<<  (tEND - tSTART) / CLOCKS_PER_SEC  <<" sec"<<endl;
-  	// #3
-    tSTART=clock();
-    exec("traceroute 168.95.1.1");
-    tEND=clock();
-    cout<<"#3 trace "<<  (tEND - tSTART) / CLOCKS_PER_SEC  <<" sec"<<endl;
-
-	
-	cout<<endl;
-	tSTART=clock();
-    // thread
-    auto ping1 = thread(exec, "traceroute 1.1.1.1");
-    auto ping2 = thread(exec, "traceroute 8.8.8.8");
-    auto ping3 = thread(exec, "traceroute 168.95.1.1");
-    ping1.join();
-    ping2.join();
-    ping3.join();
-	tEND=clock();
-    cout<<"using thead "<<  (tEND - tSTART) / CLOCKS_PER_SEC  <<" sec"<<endl;
-
-    return 0;
 }
